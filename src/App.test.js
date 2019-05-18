e2f648e9fc1117b0f0de5d8a2806fc2ac6fc85f1
yattom <tsutomu.yasui@gmail.com>
2019-05-18 17:01:02 +0900

    refactoring tests

--- src/App.test.js
+++ src/App.test.js
@@ -26,17 +26,19 @@ describe('Board', () => {
       );
     });
   });
+});
 
+describe('playing game', () => {
   describe('moving walker', function () {
-    it('select piece', () => {
+    it('select piece and move', () => {
       const wrapper = mount(<Board/>);
       expect(wrapper.find(Walker).length).toBe(2);
       const walker = wrapper.find(Walker).at(0);
       expect(walker.parents(Cell).key()).toBe("(1,0)");
       walker.simulate('click');
 
-      const cell_1_1 = wrapper.find(Cell).findWhere(n => n.key() === "(0,0)");
-      cell_1_1.simulate('click');
+      const cell_0_0 = wrapper.find(Cell).findWhere(n => n.key() === "(0,0)");
+      cell_0_0.simulate('click');
       const walker_after = wrapper.find(Walker).at(0);
       expect(walker_after.parents(Cell).key()).toBe("(0,0)");
     });
